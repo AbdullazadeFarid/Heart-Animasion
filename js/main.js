9e5820847fccd7d94dd502635f4e6ca25cdf3fba
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-17 00:04:33 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var btn = document.querySelector(".btn")
+
+var around = document.querySelector(".around")
+
+
+
+
+
+btn.addEventListener('click', animate);
+
+
+
+function animation() {
+
+
+  around.style.transform = 'scale(2)';  // around div elementinin ölçüsünü iki dəfə artır
+
+  setTimeout(function() {
+    around.style.transform = 'scale(1)';              //// 1300 millisaniyə (1.3 saniyə) sonra around div elementinin ölçüsünü orijinal həddinə qaytarır
+  }, 1300);
+}
